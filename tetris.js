3e74553132c56cc60e7c6734b96080962570b38d
Viljami Ruokonen <vruokonen@gmail.com>
2019-10-18 21:01:24 +0300

    made eraseTile function to tile object and check for matrix if matrix is full

--- tetris.js
+++ tetris.js
@@ -1,6 +1,7 @@
 const box = 40;
 const height = 680;
 const width = 400;
+var gameOn = true;
 
 function matrix() {
     this.coordinates = [];
@@ -17,8 +18,34 @@ function matrix() {
 
     this.insertTileToCoordinates = (tile) => {
         tile.forEach(element => {
-            this.coordinates[element[0]][element[1]] =1;
+            this.coordinates[element[0]][element[1]] = 1;
         });
+    }
+
+    this.drawGrid = () => {
+        let canvas = document.getElementById('tetris');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            for (let y = 0; y < height; y += box) {
+                for (let x = 0; x < width; x += box) {
+                    ctx.strokeRect(x, y, box, box);
+                }
+            }
+        }
+    }
+    this.matrixIsFull = () => {
+        for(let j = 0; j<this.coordinates[0].length; j++){
+            let counter = 0;
+            for(let i = 0; i<this.coordinates.length; i++){
+                if(this.coordinates[i][j] == 1){
+                    counter++;
+                }
+                if(counter>16){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
@@ -44,9 +71,9 @@ function tile() {
         for (let i = 0; i < this.coord.length; i++) {
             let arrayY = this.coord[i][0];
             let arrayX = this.coord[i][1];
-            console.log(arrayY, arrayX);
-            console.log(array_of_arrays[arrayY][arrayX]);
-            if (array_of_arrays[arrayY][arrayX] == 1) {
+            //console.log(arrayY, arrayX);
+            //console.log(array_of_arrays[arrayY][arrayX]);
+            if (arrayY> 15 || array_of_arrays[arrayY][arrayX] == 1) {
                 console.log('true')
                 return true;
             }
@@ -58,23 +85,53 @@ function tile() {
         var canvas = document.getElementById('tetris');
         if (canvas.getContext) {
             var ctx = canvas.getContext('2d');
-            this.coord.forEach(crd  => {
-                ctx.fillRect(crd[1]*box, crd[0]*box, box, box); 
+            this.coord.forEach(crd => {
+                ctx.fillRect(crd[1] * box, crd[0] * box, box, box);
+            });
+        }
+    }
+
+    this.eraseTile = () => {
+        var canvas = document.getElementById('tetris');
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            this.coord.forEach(crd => {
+                ctx.clearRect(crd[1] * box, crd[0] * box, box, box);
             });
         }
     }
 }
 
-let tiili = new tile();
-let m = new matrix();
-console.log(tiili.getCoordinates());
 
-
-/* setInterval(() => {
-    if(tile){
-        y = moveTile(y);
+//Game itself
+var ruudukko = new matrix();
+ruudukko.makeMatrix();
+ruudukko.drawGrid();
+var counter = 1;
+var tiili = new tile();
+while(gameOn) {
+    tiili.drawTile();
+    if(tiili.collision(ruudukko.coordinates)) {
+        ruudukko.insertTileToCoordinates(tiili.getCoordinates());
+        tiili = new tile();
+    } else {
+        tiili.eraseTile();
+        tiili.gravity();
+        ruudukko.drawGrid();
     }
-}, 1000); */
+    
+    console.log(ruudukko.matrixIsFull());
+    if(ruudukko.matrixIsFull()) {
+        gameOn = false;
+    }
+    console.log(counter);
+    counter++;
+}
+
+        
+ 
+
+
 
 
 
